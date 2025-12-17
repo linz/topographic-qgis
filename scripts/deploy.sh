718b418a93172f84f252ab4fd1c1f754e25655b4
Wentao Kuang <wt.kuang@icloud.com>
2025-12-17 15:11:33 +1300

    Deploy the qgis file into s3

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run the topographic-system/map container with all the extra environment vars and args passed in
+docker run \
+  --entrypoint node \
+  -v ${PWD}:${PWD} \
+  -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_REGION -e AWS_DEFAULT_REGION \
+  --rm \
+  ghcr.io/linz/topographic-system/map:pr-16 \
+  map \
+  deploy \
+  "$@"
