ef1a8e1952f062cc5be0263f076dd767d5b9e5c4
Wentao Kuang <wt.kuang@icloud.com>
2026-01-21 11:08:19 +1300

    Test on container pr 25

--- scripts/topographic-system.sh
+++ scripts/topographic-system.sh
@@ -5,5 +5,5 @@ docker run \
   --rm \
   -v ${PWD}:${PWD} \
   -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e AWS_SESSION_TOKEN -e AWS_REGION -e AWS_DEFAULT_REGION \
-  ghcr.io/linz/topographic-system/map:pr-30 \
+  ghcr.io/linz/topographic-system/map:pr-25 \
   "$@"
