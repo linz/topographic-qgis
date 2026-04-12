98d9c2661c19d7a216d8002679085c6babe9ea82
ccbblin <205563055+ccbblin@users.noreply.github.com>
2026-04-13 11:54:25 +1200

    latest kart container

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,6 +4,6 @@
 docker run \
   --rm \
   -v ${PWD}:${PWD} \
-  ghcr.io/linz/topographic-system/kart:git-5a58682805b14440f857b9c204adc7e9b04c4024 \
+  ghcr.io/linz/topographic-system/kart:git-518ffda6ac0342d7efc21a4c6d4e4681d9fc5f34 \
   lint-qgis \
   --qgis "$@"
